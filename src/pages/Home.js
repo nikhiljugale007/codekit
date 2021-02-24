fab1c2d6d7d58b2e9a883f37ec1282a65dcf205a
Nikhil Juagale <nikhiljugale007>
2021-02-24 10:05:01 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home1 from "../components/Home";
 import Services from "../components/Services";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Home = () => {
 	return (
 		<>
+			<Header />
 			<Home1 />
 			<Services />
 			<Footer />
